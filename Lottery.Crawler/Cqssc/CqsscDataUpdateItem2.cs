fb21a37fbfea53f6845fcc5fc08ce617d03dc8e3
liuhll <1029765111@qq.com>
2018-05-14 01:59:33 +0800

    新增重庆时时彩开奖数据源

---
+++ Lottery.Crawler/Cqssc/CqsscDataUpdateItem2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using EasyHttp.Http;
+using Lottery.Dtos.Lotteries;
+using Lottery.Infrastructure.Extensions;
+
+namespace Lottery.Crawler.Cqssc
+{
+    public class CqsscDataUpdateItem2 : BaseDataUpdateItem
+    {
+        public CqsscDataUpdateItem2(DataSiteDto dataSite) : base(dataSite)
+        {
+        }
+
+        protected override IList<LotteryDataDto> RequestDatas(int finalData)
+        {
+            _httpClient.Request.Accept = HttpContentTypes.ApplicationXml;
+            _httpClient.Request.UserAgent = CHROME_UA;
+            var response = _httpClient.Get(_dataSite.Url);
+            var crawlData = response.RawText;
+            crawlData = crawlData.Replace("gb2312", "utf-8");
+            var xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(crawlData);
+
+            var lotteryDatas = xmlDoc.SelectNodes("/xml/row");
+            if (lotteryDatas != null)
+            {
+                var resultList = new List<LotteryDataDto>();
+                foreach (XmlNode item in lotteryDatas)
+                {
+                    var periodStr = item.Attributes["expect"].Value.Substring(2);
+                    var period = Convert.ToInt32(periodStr);
+                    if (period > finalData)
+                    {
+                        var lotteryData = new LotteryDataDto()
+                        {
+                            LotteryId = _dataSite.LotteryId,
+                            Period = period,
+                            LotteryTime = Convert.ToDateTime(item.Attributes["opentime"].Value),
+                            Data = item.Attributes["opencode"].Value
+                        };
+                        resultList.Add(lotteryData);
+                    }
+
+                }
+                return resultList;
+            }
+           
+            return null;
+        }
+
+    }
+}
---
+++ Lottery.Crawler/Cqssc/CqsscDataUpdateItem3.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using EasyHttp.Http;
+using Lottery.Dtos.Lotteries;
+using Lottery.Infrastructure.Extensions;
+
+namespace Lottery.Crawler.Cqssc
+{
+    public class CqsscDataUpdateItem3 : BaseDataUpdateItem
+    {
+        public CqsscDataUpdateItem3(DataSiteDto dataSite) : base(dataSite)
+        {
+        }
+
+        protected override IList<LotteryDataDto> RequestDatas(int finalData)
+        {
+            _httpClient.Request.Accept = HttpContentTypes.ApplicationJson;
+            _httpClient.Request.UserAgent = CHROME_UA;
+            var response = _httpClient.Get(_dataSite.Url);
+            var crawlData = response.DynamicBody;
+            if (crawlData != null && crawlData.errorCode == 0)
+            {
+                var resultList = new List<LotteryDataDto>();
+                var lotteryDatas = crawlData.result.data;
+                foreach (var data in lotteryDatas)
+                {
+                    string periodStr = data.preDrawIssue.ToString();
+                    periodStr = periodStr.Substring(2);
+                    var period = Convert.ToInt32(periodStr);
+                    if (period > finalData)
+                    {
+                        var lotteryData = new LotteryDataDto()
+                        {
+                            LotteryId = _dataSite.LotteryId,
+                            Period = period,
+                            LotteryTime = Convert.ToDateTime(data.preDrawTime),
+                            Data = data.preDrawCode
+                        };
+                        resultList.Add(lotteryData);
+                    }
+
+
+                }
+
+                return resultList;
+            }
+           
+            return null;
+        }
+
+    }
+}
